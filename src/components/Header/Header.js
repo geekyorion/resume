53c003fc1eedd615ecfa78ad99e78c50dcb3bcc7
Shashank Sharma <shashank.ns67@gmail.com>
2022-06-14 15:20:37 +0530

    header component is completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { HEADER_CONTACT_ICONS, HEADER_LINKS_ICONS} from '../../utils/icons-library';
+import './Header.css';
 
 const Header = ({ data }) => {
   const [headerState] = useState(data);
@@ -6,44 +8,52 @@ const Header = ({ data }) => {
   return (
     <div className="header-section">
       <div className="main-info">
-        <div className="bio-wrapper">
-          <div className="name-text">{ headerState.name }</div>
-          <div className="role-text">{ headerState.role }</div>
-          <div className="bio-text">{ headerState.bio }</div>
-        </div>
-
-        {headerState.image?.showImage && (
-          <div className="image-wrapper">
-            <img src={headerState.image.url} alt={`profile of ${headerState.name}`} className="profile-picture" />
+        <div className="bio-and-image">
+          <div className="bio-wrapper">
+            <div className="name-text">{ headerState.name }</div>
+            <div className="role-text">{ headerState.role }</div>
+            <div className="bio-text">{ headerState.bio }</div>
           </div>
-        )}
 
-        <div className="contacts-wrapper">
-          {headerState.contact.filter(contact => !contact.isHidden).map(contact => (
-            <div className="contact">
-              <div className="contact-icon">{ contact.icon }</div>
-              <div className="contact-text">
-                {contact.type === 'email' ? (
-                  <a className="email-text" href={`mailto:${contact.value}`} rel="noreferrer noopener">{ contact.value }</a>
-                ) : contact.type === 'phone' ? (
-                  <a className="phone-text" href={`tel:${contact.value.replace(/\s+/g, '')}`} rel="noreferrer noopener">{ contact.value }</a>
-                ) : (
-                  <p classname="other-contact-text">{ contact.value }</p>
-                )}
-              </div>
+          {headerState.image?.showImage && (
+            <div className="image-wrapper">
+              <img src={headerState.image.url} alt={`profile of ${headerState.name}`} className="profile-picture" />
             </div>
-          ))}
+          )}
         </div>
-
-        <div className="links-wrapper">
-          {headerState.links.filter(link => !link.isHidden).map(link => (
-            <div className="link">
-              <div className="link-icon">{ link.icon }</div>
-              <div className="link-text">
-                <a href={link.value} target="_blank" rel="noreferrer noopener">{ link.type }</a>
+        
+        <div className="all-links">
+          <div className="contacts-wrapper">
+            {headerState.contact.filter(contact => !contact.isHidden).map(contact => (
+              <div className="contact">
+                <div className={`contact-icon ${contact.iconClass ?? ''}`}>{ HEADER_CONTACT_ICONS[contact.icon] }</div>
+                <div className="contact-text">
+                  {contact.type === 'email' ? (
+                    <a className="email-text" href={`mailto:${contact.value}`} rel="noreferrer noopener">{ contact.value }</a>
+                  ) : contact.type === 'phone' ? (
+                    <a className="phone-text" href={`tel:${contact.value.replace(/\s+/g, '')}`} rel="noreferrer noopener">{ contact.value }</a>
+                  ) : contact.type === 'website' ? (
+                    <a className="website-text" href={contact.link} rel="noreferrer noopener">{ contact.value }</a>
+                  ) : (
+                    <p classname="other-contact-text">{ contact.value }</p>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+
+          <hr className="link-hr" />
+          
+          <div className="links-wrapper all-links-wrapper">
+            {headerState.links.filter(link => !link.isHidden).map(link => (
+              <div className="link">
+                <div className={`link-icon ${link.iconClass ?? ''}`}>{ HEADER_LINKS_ICONS[link.icon] }</div>
+                <div className="link-text">
+                  <a href={link.value} target="_blank" rel="noreferrer noopener">{ link.type }</a>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
