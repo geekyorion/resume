909af8549b4c13eac2a7f3759def06b6656c37a4
Shashank Sharma <shashank.ns67@gmail.com>
2022-06-24 17:54:06 +0530

    project section added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import './Projects.css';
+
 const Projects = ({ data }) => {
-  console.log(data);
+  const [projectState] = useState(data);
 
   return (
-    <div>Projects</div>
+    <div className="project-wrapper">
+      <h3 className="project-header-text">Projects</h3>
+      {projectState.map((project, index) => (
+        <div className="project" key={`project-${index}`}>
+          <div className="project-name">{project.name}</div>
+          <div className="project-desc">{project.desc}</div>
+          <div className="project-technologies">
+            <div className="project-technologies-label">Technologies Used: </div>
+            <div className="project-technologies-values">
+              {project.technologies.join(', ')}
+            </div>
+          </div>
+          <div className="project-link">
+            [ <a href={project.link} target="_blank" rel="noopener noreferrer">{project.link}</a> ]
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
