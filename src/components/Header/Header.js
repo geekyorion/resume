dfa78eb935d52df9f6c51782392a1957d64ffcf5
Shashank Sharma <shashank.ns67@gmail.com>
2022-06-24 12:49:33 +0530

    experience section added with minor bug resolved

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,9 @@ import './Header.css';
 const Header = ({ data }) => {
   const [headerState] = useState(data);
 
+  // fetch the current profile picture name [user can save more than one]
+  const headerImage = require(`../../assets/images/${headerState.image.imageName}`);
+
   return (
     <div className="header-section">
       <div className="main-info">
@@ -14,10 +17,9 @@ const Header = ({ data }) => {
             <div className="role-text">{ headerState.role }</div>
             <div className="bio-text">{ headerState.bio }</div>
           </div>
-
           {headerState.image?.showImage && (
             <div className="image-wrapper">
-              <img src={headerState.image.url} alt={`profile of ${headerState.name}`} className="profile-picture" />
+              <img src={headerImage} alt={`profile of ${headerState.name}`} className="profile-picture" />
             </div>
           )}
         </div>
@@ -25,7 +27,7 @@ const Header = ({ data }) => {
         <div className="all-links">
           <div className="contacts-wrapper">
             {headerState.contact.filter(contact => !contact.isHidden).map(contact => (
-              <div className="contact">
+              <div className="contact" key={`contact-${contact.type}`}>
                 <div className={`contact-icon ${contact.iconClass ?? ''}`}>{ HEADER_CONTACT_ICONS[contact.icon] }</div>
                 <div className="contact-text">
                   {contact.type === 'email' ? (
@@ -35,7 +37,7 @@ const Header = ({ data }) => {
                   ) : contact.type === 'website' ? (
                     <a className="website-text" href={contact.link} rel="noreferrer noopener">{ contact.value }</a>
                   ) : (
-                    <p classname="other-contact-text">{ contact.value }</p>
+                    <p className="other-contact-text">{ contact.value }</p>
                   )}
                 </div>
               </div>
@@ -46,7 +48,7 @@ const Header = ({ data }) => {
           
           <div className="links-wrapper all-links-wrapper">
             {headerState.links.filter(link => !link.isHidden).map(link => (
-              <div className="link">
+              <div className="link" key={`link-${link.type}`}>
                 <div className={`link-icon ${link.iconClass ?? ''}`}>{ HEADER_LINKS_ICONS[link.icon] }</div>
                 <div className="link-text">
                   <a href={link.value} target="_blank" rel="noreferrer noopener">{ link.type }</a>
