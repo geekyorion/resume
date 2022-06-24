52d640fda44b86123cda40cc3fe66c188cfd698a
Shashank Sharma <shashank.ns67@gmail.com>
2022-06-24 22:34:19 +0530

    extra section is added with all 6 sub sections

--- src/components/Extra/Extra.js
+++ src/components/Extra/Extra.js
@@ -1,8 +1,25 @@
+import Skills from "./Skills/Skills";
+import Certifications from "./Certifications/Certifications";
+import Achievements from "./Achievements/Achievements";
+import Tools from "./Tools/Tools";
+import Languages from "./Languages/Languages";
+import Interests from "./Interests/Interests";
+import { isEmpty } from "../../utils/isEmpty";
+import './Extra.css';
+
 const Extra = ({ data }) => {
-  console.log(data);
   
+  const { skills, certificates, achievements, softwares, interests, languages, showOnly } = data;
+
   return (
-    <div>Extra</div>
+    <div className="extras-wrapper">
+      {!isEmpty(skills) && showOnly['skills'] && <Skills data={skills} />}
+      {!isEmpty(certificates) && showOnly['certificates'] && <Certifications data={certificates} />}
+      {!isEmpty(achievements) && showOnly['achievements'] && <Achievements data={achievements} />}
+      {!isEmpty(softwares) && showOnly['softwares'] && <Tools data={softwares} />}
+      {!isEmpty(interests) && showOnly['interests'] && <Interests data={interests} />}
+      {!isEmpty(languages) && showOnly['languages'] && <Languages data={languages} />}
+    </div>
   );
 };
 
