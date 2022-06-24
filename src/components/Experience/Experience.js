c2c2f85683c3532070dfa2999d2e5f58d018ae8a
Shashank Sharma <shashank.ns67@gmail.com>
2022-06-24 12:49:33 +0530

    experience section added with minor bug resolved

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,8 +1,72 @@
-const Experinece = ({ data }) => {
-  console.log(data);
+import { useState } from 'react';
+import { getDateFormat } from '../../utils/dates';
+import './Experience.css';
 
+const Experinece = ({ data }) => {
+
+  const [expState] = useState(data);
+  
   return (
-    <div>Experinece</div>
+    <div className="experience-wrapper">
+      <h3 className="experience-header-text">Work Experience</h3>
+      {expState.map((exp, index) => (
+        <div className="experience" key={`exp-${index}`}>
+          <p className="company-name">{exp.company}</p>
+          <div className="exp-info">
+            <p className="exp-duration">
+              {getDateFormat(exp.duration.from, 'MMM, YYYY')}
+              {' - '}
+              {exp.duration.to === 'present'
+                ? 'Present'
+                : getDateFormat(exp.duration.to, 'MMM, YYYY')
+              }
+            </p>
+            <p>{exp.location}</p>
+          </div>
+          
+          {exp.work.map((work, workIndex) => (
+            <div className="work-wrapper" key={`exp-${index}-work-${workIndex}`}>
+              <div className="work-info">
+                <p className="work-role">{work.role}</p>
+                <p className="exp-duration">
+                  {getDateFormat(work.duration.from, 'MMM, YYYY')}
+                  {' - '}
+                  {work.duration.to === 'present'
+                    ? 'Present'
+                    : getDateFormat(work.duration.to, 'MMM, YYYY')
+                  }
+                </p>
+              </div>
+              {work.showDescription && (
+                <div className="work-desc">{ work.description }</div>
+              )}
+              <div className="projects">
+                <ul className="project-list">
+                  {work.projects.map((project, projectIndex) => (
+                    <li className="project" key={`project-${project.name}-${projectIndex}`}>
+                      <div className="project-name">{project.name}</div>
+                      <div className="project-technologies">
+                        <div className="project-technologies-label">Technology Used: </div>
+                        <div className="project-technologies-values">
+                          {project.technologies.join(', ')}
+                        </div>
+                      </div>
+                      <ul className="responsibilties">
+                        {project.responsibilities.map((resp, respIndex) => (
+                          <li className="project-responsibilty" key={`project-${project.name}-${projectIndex}-${respIndex}`}>
+                            {resp}
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 };
 
